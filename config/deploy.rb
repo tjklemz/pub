91dfade02b7e0b3d87c226c04a319f2c718e9e30
Thomas Klemz <thomas.klemz@creoagency.com>
2013-06-21 15:43:45 -0700

    cap deploy works

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,5 +11,20 @@ set :ssh_options, { :forward_agent => true }
 require 'capistrano/ext/multistage'
 
 require 'capistrano/nginx/tasks'
+
 after "deploy:setup", "nginx:setup", "nginx:reload"
+after "deploy:setup" do 
+    run "#{sudo} chown -R #{user} #{deploy_to}"
+end
+
+set :uwsgi_log_dir, "/var/log/uwsgi"
+set(:uwsgi_log) { "#{uwsgi_log_dir}/#{application}.log" }
+
+before "deploy:restart" do
+    run "cd #{current_release} && #{sudo} pip install -r requirements.txt"
+#    run "cd #{current_release} && python manage.py migrate && python manage.py syncdb"
+    run "#{sudo} mkdir -p #{uwsgi_log_dir}"
+    run "#{sudo} touch #{uwsgi_log}"
+    run "#{sudo} uwsgi --chdir #{current_release} --socket :8001 --wsgi-file wsgi.py --daemonize #{uwsgi_log}"
+end
 
